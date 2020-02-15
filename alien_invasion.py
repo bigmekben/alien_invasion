176d949a5aae1a6c0dda1fc909341206c204098d
Benjamin Thompson <bigmekben@gmail.com>
2020-02-15 13:49:49 -0500

    Delete old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            
+            # Git rid of bullets that have gone offscreen.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            
             self._update_screen()
             
     def _check_events(self):
